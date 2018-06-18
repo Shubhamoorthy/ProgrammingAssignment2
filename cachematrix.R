62aa5a2c2c8908d27ecf8d49b73c3d9f3331b742
Shubhamoorthy Gunaseelan <gshubhamoorthy@gmail.com>
2018-06-18 12:45:43 +0530

    Invers of a matrix for assignment 2 week 3 course 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inv.cache <- NULL
+  set <- function(y) {
+    x <<- y
+    inv.cache <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse.mat) inv.cache <<- inverse.mat
+  getinv <- function() inv.cache
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 
 }
 
@@ -12,4 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv.req <- x$getinv()
+  if(!is.null(inv.req)) {
+    message("getting cached data")
+    return(inv.req)
+  }
+  data <- x$get()
+  inv.req <- solve(data, ...)
+  x$setinv(inv.req)
+  inv.req
+  
 }
